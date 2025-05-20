717608347e3f403c3bda22032304e7894c933cde
Dustin Spicuzza <dustin@virtualroadside.com>
2025-05-20 00:17:58 -0400

    Migrate to semiwrap

--- playingwithfusion/pwf.cpp
+++ playingwithfusion/pwf.cpp
@@ -1,3 +1,3 @@
-#include <rpygen_wrapper.hpp>
+#include <semiwrap_init.playingwithfusion._playingwithfusion.hpp>
 
-RPYBUILD_PYBIND11_MODULE(m) { initWrapper(m); }
+SEMIWRAP_PYBIND11_MODULE(m) { initWrapper(m); }
